9594714bbf29830bd6d2d8fb0f2e46d6401446d6
Nek <trunks_17_54@hotmail.com>
2023-09-15 18:09:12 -0600

    financecalculator

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,15 @@
 import { useState, useEffect } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
-const ControlPresupuesto = ({ gastos, presupuesto }) => {
+const ControlPresupuesto = ({
+  gastos,
+  setGastos,
+  presupuesto,
+  setPresupuesto,
+  setIsValidPresupuesto,
+}) => {
+  const [porcentaje, setPorcentaje] = useState(20);
   const [disponible, setDisponible] = useState(0);
   const [gastado, setGastado] = useState(0);
 
@@ -10,8 +19,17 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
       0
     );
     const totalDisponible = presupuesto - totalGastado;
+
+    const nuevoPorcentaje = (
+      ((presupuesto - totalDisponible) / presupuesto) *
+      100
+    ).toFixed(2);
+
     setGastado(totalGastado);
     setDisponible(totalDisponible);
+    setTimeout(() => {
+      setPorcentaje(nuevoPorcentaje);
+    }, 1000);
   }, [gastos]);
 
   const formatearCantidad = (cantidad) => {
@@ -21,17 +39,37 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
     });
   };
 
+  const handleResetApp = () => {
+    const resultado = confirm("¿Deseas reiniciar presupuesto y gasto?");
+    if (resultado) {
+      setGastos([]);
+      setPresupuesto(0);
+      setIsValidPresupuesto(false);
+    }
+  };
+
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
-        <p>Grafica aqui</p>
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: porcentaje > 100 ? "#DC2626" : "#3b82f6",
+            trailColor: "#f5f5f5",
+            textColor: porcentaje > 100 ? "#DC2626" : "#3b82f6",
+          })}
+          value={porcentaje}
+          text={`${porcentaje}% Gastado`}
+        />
       </div>
 
       <div className="contenido-presupuesto">
+        <button className="reset-app" type="button" onClick={handleResetApp}>
+          Resetear App
+        </button>
         <p>
           <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
         </p>
-        <p>
+        <p className={`${disponible < 0 ? "negativo" : ""}`}>
           <span>Disponible: </span> {formatearCantidad(disponible)}
         </p>
         <p>
--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,14 +1,44 @@
 import React from "react";
 import Gasto from "./Gasto";
 
-const ListadoGastos = ({ gastos, setGastoEditar }) => {
+const ListadoGastos = ({
+  gastos,
+  setGastoEditar,
+  eliminarGasto,
+  filtro,
+  gastosFiltrados,
+}) => {
   return (
     <div className="listado-gastos contenedor">
-      <h2>{gastos.length ? "Gastos" : "No Hay Gastos Aún"}</h2>
-
-      {gastos.map((gasto) => (
-        <Gasto key={gasto.id} gasto={gasto} setGastoEditar={setGastoEditar} />
-      ))}
+      {filtro ? (
+        <>
+          <h2>
+            {gastosFiltrados.length
+              ? "Gastos"
+              : "No Hay Gastos En Esta Categoría"}
+          </h2>
+          {gastosFiltrados.map((gasto) => (
+            <Gasto
+              key={gasto.id}
+              gasto={gasto}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2>{gastos.length ? "Gastos" : "No Hay Gastos Aún"}</h2>
+          {gastos.map((gasto) => (
+            <Gasto
+              key={gasto.id}
+              gasto={gasto}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 };
